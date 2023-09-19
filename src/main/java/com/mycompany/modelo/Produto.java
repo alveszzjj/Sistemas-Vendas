625aea7d32626080f3ff8ae498e717ca2da07582
JOAO OTAVIO DOS SANTOS ALVES <joao.2922@aluno.pr.senac.br>
2023-09-19 14:59:08 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/modelo/Produto.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo;
+
+/**
+ *
+ * @author joao.2922
+ */
+public class Produto {
+    private String nome;
+    private Double preço;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreço() {
+        return preço;
+    }
+
+    public void setPreço(Double preço) {
+        this.preço = preço;
+    }
+}
